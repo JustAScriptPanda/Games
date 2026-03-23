cfdcacfbf920f8da3ec47334aefcadd1c6e31e59
JustAScriptPanda <anakjawa10000@gmail.com>
2026-03-23 09:23:04 +0700

    Add initial script for UGC game setup

---
+++ UGC Games/71820385183232.lua
@@ -0,0 +1,45 @@
+--[[
+🕌🕋𝑪𝒆𝒏𝒕𝒓𝒂𝒍 𝑳𝒊𝒎𝒊𝒕𝒆𝒅 𝑼𝑮𝑪 🕋🕌
+✧ 𝐢𝐝𝐮𝐥 𝐟𝐢𝐭𝐫𝐢 𝐭𝐡𝐞𝐦𝐞 ✧
+
+`Now!`
+
+🕋 𝑳𝑰𝑵𝑲 𝑰𝑻𝑬𝑴 🕋
+- *https://www.roblox.com/catalog/80403225632312/Perak*
+
+🕌 𝑳𝑰𝑵𝑲 𝑴𝑨𝑷 🕌
+- *https://www.roblox.com/games/71820385183232/Serem*
+
+🐄 𝑯𝑶𝑾 𝑻𝑶 𝑮𝑬𝑻 🐄
+- Get 12.000 Point 
+- Need Survival 500 Survival!
+
+🐐 𝑺𝑻𝑶𝑪𝑲 🐐
+- *20*
+
+🍪 𝑵𝑶𝑻𝑬 🍪
+- -
+
+✧🕌°:₊˚.⋆☾📿⋆⁺₊✧
+--]]
+
+-- made by alwi / velocity x
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    game:GetService("VirtualUser"):CaptureController()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
+game:GetService("Players").LocalPlayer.Character:PivotTo(CFrame.new(-669.153442, -4.075121, -7.740437))
+
+Instance.new("Part", workspace)
+workspace:FindFirstChildOfClass("Part").Size = Vector3.new(100,1,100)
+workspace:FindFirstChildOfClass("Part").CFrame = CFrame.new(-664.837158, -12.995497, -11.279767)
+workspace:FindFirstChildOfClass("Part").Anchored = true
+workspace:FindFirstChildOfClass("Part").Material = Enum.Material.SmoothPlastic
+workspace:FindFirstChildOfClass("Part").Color = Color3.fromRGB(0,0,0)
+workspace:FindFirstChildOfClass("Part").Transparency = 0.2
+
+Instance.new("PointLight", workspace:FindFirstChildOfClass("Part"))
+workspace:FindFirstChildOfClass("PointLight").Brightness = 2
+workspace:FindFirstChildOfClass("PointLight").Range = 10
+workspace:FindFirstChildOfClass("PointLight").Color = Color3.fromRGB(255,255,255)
