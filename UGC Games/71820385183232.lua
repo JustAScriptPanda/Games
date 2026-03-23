5ac1c4cbd6ae5f14d154f58108e6ab7f483b26fb
JustAScriptPanda <anakjawa10000@gmail.com>
2026-03-23 09:55:03 +0700

    Refactor teleport logic and light settings
    Removed teleport loop and adjusted light properties.

--- UGC Games/71820385183232.lua
+++ UGC Games/71820385183232.lua
@@ -24,11 +24,42 @@
 --]]
 
 -- made by alwi / velocity x
+--[[
+🕌🕋𝑪𝒆𝒏𝒕𝒓𝒂𝒍 𝑳𝒊𝒎𝒊𝒕𝒆𝒅 𝑼𝑮𝑪 🕋🕌
+✧ 𝐢𝐝𝐮𝐥 𝐟𝐢𝐭𝐫𝐢 𝐭𝐡𝐞𝐦𝐞 ✧
+
+`Now!`
+
+🕋 𝑳𝑰𝑵𝑲 𝑰𝑻𝑬𝑴 🕋
+- *https://www.roblox.com/catalog/80403225632312/Perak*
+
+🕌 𝑳𝑰𝑵𝑲 𝑴𝑨𝑷 🕌
+- *https://www.roblox.com/games/71820385183232/Serem*
+
+🐄 𝑯𝑶𝑾 𝑻𝑶 𝑮𝑬𝑻 🐄
+- Get 12.000 Point 
+- Need Survival 500 Survival!
+
+🐐 𝑺𝑻𝑶𝑪𝑲 🐐
+- *20*
+
+🍪 𝑵𝑶𝑻𝑬 🍪
+- -
+
+✧🕌°:₊˚.⋆☾📿⋆⁺₊✧
+--]]
+
+-- made by alwi / velocity x
 -- Anti AFK
 game:GetService("Players").LocalPlayer.Idled:Connect(function()
     game:GetService("VirtualUser"):CaptureController()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
 end)
+
+-- Teleport
+game:GetService("Players").LocalPlayer.Character:PivotTo(
+    CFrame.new(-669.153442, -4.075121, -7.740437)
+)
 
 -- Create Part
 Instance.new("Part", workspace)
@@ -44,10 +75,3 @@ Instance.new("PointLight", workspace:GetChildren()[#workspace:GetChildren()])
 workspace:GetChildren()[#workspace:GetChildren()].Brightness = 2
 workspace:GetChildren()[#workspace:GetChildren()].Range = 10
 workspace:GetChildren()[#workspace:GetChildren()].Color = Color3.fromRGB(255,255,255)
-
--- Teleport
-while wait() do
-game:GetService("Players").LocalPlayer.Character:PivotTo(
-    CFrame.new(-669.153442, -4.075121, -7.740437)
-)
-end
